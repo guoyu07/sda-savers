ad260ec732d1b2176afbbe0af4f3bc8ff5346dfa
jianlai2007 <jianlai2007@qq.com>
2017-09-08 16:30:13 +0800

    '搭建框架'

---
+++ src/main/java/com/sda/savers/SdaSaversApplication.java
@@ -0,0 +1,12 @@
+package com.sda.savers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaSaversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaSaversApplication.class, args);
+	}
+}
